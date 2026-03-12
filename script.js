6df6f5237c9f2e773cdd9359926118c82cc0c535
beyond <beyondarchitectofficial@gmail.com>
2026-03-12 22:49:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-function updateClocks(){
+function updateClocks() {
+    let now = new Date();
 
-let now = new Date();
+    document.getElementById("athens").textContent =
+        now.toLocaleTimeString("en-GB", { timeZone: "Europe/Athens" });
 
-document.getElementById("athens").textContent =
-now.toLocaleTimeString("en-GB",{timeZone:"Europe/Athens"});
+    document.getElementById("sydney").textContent =
+        now.toLocaleTimeString("en-GB", { timeZone: "Australia/Sydney" });
 
-document.getElementById("sydney").textContent =
-now.toLocaleTimeString("en-GB",{timeZone:"Australia/Sydney"});
+    document.getElementById("jakarta").textContent =
+        now.toLocaleTimeString("en-GB", { timeZone: "Asia/Jakarta" });
 
-document.getElementById("jakarta").textContent =
-now.toLocaleTimeString("en-GB",{timeZone:"Asia/Jakarta"});
+    document.getElementById("newyork").textContent =
+        now.toLocaleTimeString("en-GB", { timeZone: "America/New_York" });
 
-document.getElementById("newyork").textContent =
-now.toLocaleTimeString("en-GB",{timeZone:"America/New_York"});
+    document.getElementById("paris").textContent =
+        now.toLocaleTimeString("en-GB", { timeZone: "Europe/Paris" });
 
-document.getElementById("paris").textContent =
-now.toLocaleTimeString("en-GB",{timeZone:"Europe/Paris"});
-
-
-document.getElementById("california").textContent = now.toLocaleTimeString("en-GB",{timeZone:"America/Los_Angeles"});
-
+    document.getElementById("california").textContent =
+        now.toLocaleTimeString("en-GB", { timeZone: "America/Los_Angeles" });
 }
 
-setInterval(updateClocks,1000);
-updateClocks();
+// Update clocks every second
+setInterval(updateClocks, 1000);
+updateClocks(); // Initial call
